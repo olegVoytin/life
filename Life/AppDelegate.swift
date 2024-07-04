c97c7fcf32a572b1b32a877ebf1a2fd8f898e830
Oleg Voytin <ovoytin@sberhealth.ru>
2024-07-04 15:21:36 +0200

    Initial Commit

---
+++ Life/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Life
+//
+//  Created by Олег Войтин on 04.07.2024.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
